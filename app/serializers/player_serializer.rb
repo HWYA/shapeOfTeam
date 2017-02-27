0c2706392a1498308669823a1d5ec718a8a6a322
nnouns <lokaiya@gmail.com>
2017-02-26 21:26:55 -0500

    Front End V1
    datamaps (with d3 and topojson dependencies) included into index view
    player_serializer updated to include bp_latitude and bp_longitude
    fetchClubs.js up and running a la the datamaps temp map example

--- app/serializers/player_serializer.rb
+++ app/serializers/player_serializer.rb
@@ -1,3 +1,3 @@
 class PlayerSerializer < ActiveModel::Serializer
-  attributes :name, :position, :place_of_birth, :nationality
+  attributes :name, :position, :place_of_birth, :nationality, :bp_latitude, :bp_longitude
 end
