87f2372bbd8afa9090c5af7e042d2a8139afa975
nnouns <lokaiya@gmail.com>
2017-02-28 00:30:44 -0500

    fixed mistake ("Yugoslavia" instead of "Yugoslavia (Republic)") in strip_nationality method

--- app/models/player.rb
+++ app/models/player.rb
@@ -2,11 +2,11 @@ class Player < ApplicationRecord
 	belongs_to :club, optional: true
 
 	def strip_nationality
-		deprecated_nations = ["Jugoslawien (SFR)","CSSR","UDSSR","Yugoslavia"]
+		deprecated_nations = ["Jugoslawien (SFR)","CSSR","UDSSR","Yugoslavia (Republic)"]
 		if deprecated_nations.include?(self.nationality)
 			self.nationality = ""
 			self.save
 		end
 	end
-	
+
 end
