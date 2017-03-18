b6ecb6f46197a0ac9aba0d7631dac33ba70dbc33
Abner Yang <abtyang@gmail.com>
2017-03-17 22:52:04 -0400

    remove state/province/region etc. from player place of birth. ex. Modesto, Califronia is just now Modesto

--- app/models/player.rb
+++ app/models/player.rb
@@ -8,6 +8,16 @@ class Player < ApplicationRecord
 		self.place_of_birth = ""
 	end
 
+	def removes_pob_region
+		# avoid entries where place of birth is deliberately set to empty string because TransferMarkt doesn't provide one to us yet
+		if self.place_of_birth == ""
+			return
+		else
+			self.place_of_birth = self.place_of_birth.split(',')[0]
+			self.save
+		end
+	end
+
 	def strip_nationality
 		deprecated_nations = ["Jugoslawien (SFR)","CSSR","UDSSR","Yugoslavia (Republic)"]
 		# we can update nationality from another source
