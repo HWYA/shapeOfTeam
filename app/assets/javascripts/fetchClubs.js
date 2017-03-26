865751a1957e35a8d87910a20a630e2f79e10e8f
Abner Yang <abtyang@gmail.com>
2017-03-26 01:11:58 -0400

    clean up spacing, ordered css props alphanumerically

--- app/assets/javascripts/fetchClubs.js
+++ app/assets/javascripts/fetchClubs.js
@@ -1,35 +1,30 @@
-$(document).ready(function() {
+$(window).ready(function() {
   $("#get_club_name").on('change', function(event) {
 
     event.preventDefault();
 
     // encodeURIComponent converts text field params for GET
     var club_name = encodeURIComponent($("#get_club_name").val());
-    // var hostname = window.location.host
+    // to prevent mixed content issues
     if (window.location.protocol === "http:") {
-      var myDomain = "http://"+window.location.host;
+      var myDomain = "http://" + window.location.host;
     } else {
-      myDomain = "https://"+window.location.host;
+      myDomain = "https://" + window.location.host;
     }
 
     $.ajax({
-
       method: "GET",
-      url: myDomain+"/clubs?name="+club_name,
+      url: myDomain + "/clubs?name=" + club_name,
       dataType: "json",
       success: function(data) {
-        for (i=0;i<data.length;i++){
-          data[i].radius=2.5;
-          data[i].fillKey='playerFill';
-          data[i].latitude=data[i].bp_latitude;
-          data[i].longitude=data[i].bp_longitude;
+        for (i=0; i < data.length; i++) {
+          data[i].radius = 2.5;
+          data[i].fillKey = 'playerFill';
+          data[i].latitude = data[i].bp_latitude;
+          data[i].longitude = data[i].bp_longitude;
         };
-
         map.bubbles(data);
       }
-      
-
     });
-
   });
 });
